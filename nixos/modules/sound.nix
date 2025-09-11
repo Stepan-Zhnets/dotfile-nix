4f1186ecf1cc0702043f3b3b19e4f2c1c62cb028
Stepan Zhnets <90891565+Stepan-Zhnets@users.noreply.github.com>
2025-09-11 20:15:15 +0300

    update sound

--- nixos/modules/sound.nix
+++ nixos/modules/sound.nix
@@ -6,18 +6,21 @@
   # sound.enable = true;
   # hardware.pulseaudio.enable = false;
 
+  hardware.alsa.enablePersistence = true;
+
   security.rtkit.enable = true;
 
   services = {
     #=>Pipewire
     pipewire = {
       enable = true;
+      audio.enable = true;
+      jack.enable = true;
+      pulse.enable = true;
       alsa = {
         enable = true;
         support32Bit = true;
       };
-      jack.enable = true;
-      pulse.enable = true;
     };
   };
 }
