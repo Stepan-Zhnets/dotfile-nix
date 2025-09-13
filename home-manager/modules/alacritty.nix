a84508700653027bda9fd9ef89a53acc7384866c
Stepan Zhnets <90891565+Stepan-Zhnets@users.noreply.github.com>
2025-09-13 15:13:22 +0300

    update HM Alacritty

--- home-manager/modules/alacritty.nix
+++ home-manager/modules/alacritty.nix
@@ -1,19 +1,22 @@
 # ~/hone-manager/modules/alacritty.nix
+
 { pkgs, ... }:{
   programs.alacritty = {
     enable = true;
+    theme = "gruvbox_dark"; # gruvbox_light | gruvbox_dark _|_ gotham _|_ dracula _|_ catppuccin_mocha _|_
+    # package = [];
     settings = {
       window = {
         padding = {
           x = 10;
           y = 10;
         };
-        decorations = "none";
-        # opacity = 0.7
+        # decorations = "none";
+        opacity = 0.9;
       };
       scrolling.history = 1000;
       font = {
-        size = 16;
+        size = 12;
       };
     };
   };
