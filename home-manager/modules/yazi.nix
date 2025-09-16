0dae08f0fbae7ac684977942d669425d72154f98
Stepan Zhnets <90891565+Stepan-Zhnets@users.noreply.github.com>
2025-09-16 20:49:01 +0300

    update HM yazi. add ";"

--- home-manager/modules/yazi.nix
+++ home-manager/modules/yazi.nix
@@ -13,10 +13,10 @@
     theme = {
       help = {
         on = { fg = "#fe8019"; };
-        exec = { fg = "#83a598" };
-        desc = { fg = "#928374" };
-        hovered = { bg = "#504945"; bold = true };
-        footer  = { fg = "#3c3836"; bg = "#a89984" };
+        exec = { fg = "#83a598"; };
+        desc = { fg = "#928374"; };
+        hovered = { bg = "#504945"; bold = true; };
+        footer  = { fg = "#3c3836"; bg = "#a89984"; };
       };
       filetype = {
         rules = [
