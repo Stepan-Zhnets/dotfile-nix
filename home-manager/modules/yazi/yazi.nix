3ac45c2a7ec3f83bbb7180dc08a0e430951a15e9
Stepan Zhnets <90891565+Stepan-Zhnets@users.noreply.github.com>
2025-09-17 13:44:36 +0300

    update HM yazi. Add init.lua and update settings "mgr"

--- home-manager/modules/yazi/yazi.nix
+++ home-manager/modules/yazi/yazi.nix
@@ -7,8 +7,29 @@
 
   programs.yazi = {
     enable = true;
+
+    initLua = ./init.lua;
+    
     settings = {
       logo.enabled = false;
+
+      # {_MANAGER_}
+      mgr = {
+        ratio = [
+          1
+          4
+          3
+        ];
+
+        # {_SORT_}
+        sort_by = "natural";
+        sort_sensitive = true;
+        sort_reverse = false;
+        sort_dir_first = true;
+        sort_translit = true; # IF { sort_by = "natural"; };
+
+        linemode = "size_and_mtime";
+      };
     };
     theme = {
       # {_./theme/*_}
